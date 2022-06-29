6cf238337211ffbcde685ff01ad3c376dcac71db
Teagrin Forde <teagrinb@gmail.com>
2022-06-29 16:55:14 -0400

    Update timer, CSS, and Readme.md

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 //countdown timer from 90sec
 var time = document.querySelector(".timer");
-var start_time = document.getElementById('num');
-var seconds = 5;
+var seconds = 90;
 
 //Questions below
 const questions = [
@@ -228,59 +227,45 @@ function startTimer() {
       clearInterval(seconds);
       var end = prompt("Time's up!", "Enter your initials HERE");
       var initialsInput = document.getElementById(end); //collect input
-      
-      var sendInitials = document.querySelector('#champion');//send initials champion.html
-      window.addEventListener('load', () => {
-        var data = '{"Rank":"ranking", "Initials":"initialsInput", "Score":"score"}';
-        data = JSON.parse(data);
-        console.table(data);
-        var table = document.createElement("table"), row, cellA, cellB;
-        document.getElementById("container").appendChild(table);
-        for (let key in data) {
-          row = table.insertRow();
-          cellA = row.insertCell();
-          cellB = row.insertCell();        
-      }
-
-
+      //send inititals and score to champion.html
+      // var sendInitials = document.querySelector
       window.open("champion.html"); //MVP page
     }
   }, 1000);
 }
 
 
+let shuffledQuestionsEl = []; //hold shuffled questions
+let questionNumberEl = 1; //# questns opened
+let scoreEl = 0;
+let indexNumberEl = 0; //which question
 
-// let shuffledQuestions = []; //hold shuffled questions
-// let questionNumber = 1; //# questns opened
-// let score = 0;
-// let indexNumber = 0; //which question
-
-// //shuffling questions
+//shuffling questions
 // function handleQuestions() {
 //   while (seconds > 0) {
 //     const random = questions[Math.floor(Math.random() * questions.length)]
-//     if (!shuffledQuestions.includes(random)) {
-//       shuffledQuestions.push(random)
+//     if (!shuffledQuestionsEl.includes(random)) {
+//       shuffledQuestionsEl.push(random)
 //     }
 //   }
 // }
 
-// //move to the next question
-// function nextQuestion(index) {}
-//   handleQuestions()
-//   var currentQuestion = shuffledQuestions[index]
-//   document.getElementById('questionNumber').innerHTML = questionNumber
-//   document.getElementById('totalScore').innerHTML = score
-//   document.getElementById("displayQuestion").innerHTML = currentQuestion.question;
-//   document.getElementById("oneLabel").innerHTML = currentQuestion.optionA;
-//   document.getElementById("twoLabel").innerHTML = currentQuestion.optionB;
-//   document.getElementById("threeLabel").innerHTML = currentQuestion.optionC;
-//   document.getElementById("fourLabel").innerHTML = currentQuestion.optionD;
-// }
+//move to the next question
+function nextQuestion(index) {
+  handleQuestions()
+  var currentQuestion = shuffledQuestions[index]
+  document.getElementById('questionNumber').innerHTML = questionNumberEl
+  document.getElementById('totalScore').innerHTML = scoreEl
+  document.getElementById("displayQuestion").innerHTML = currentQuestion.question;
+  document.getElementById("oneLabel").innerHTML = currentQuestion.optionA;
+  document.getElementById("twoLabel").innerHTML = currentQuestion.optionB;
+  document.getElementById("threeLabel").innerHTML = currentQuestion.optionC;
+  document.getElementById("fourLabel").innerHTML = currentQuestion.optionD;
+}
 
 // //check answer function
 // function checkForAnswer() {
-//   const currentQuestion = shuffledQuestions[indexNumber] //gets current Question 
+//   const currentQuestion = shuffledQuestions[indexNumber] //gets current Question
 //   const currentQuestionAnswer = currentQuestion.correctOption //gets current Question's answer
 //   const options = document.getElementsByName("option"); //gets all elements in dom with name of 'option' (in this the radio inputs)
 //   let correctOption = null
@@ -307,7 +292,7 @@ function startTimer() {
 //       }, 1000)
 //     }
 //     else if (option.checked && option.value !== currentQuestionAnswer) {
-//       seconds-- * 10 //takes 10 seconds from time 
+//       seconds-- * 10 //takes 10 seconds from time
 //       indexNumber++
 //       setTimeout(() => {
 //         questionNumber++
